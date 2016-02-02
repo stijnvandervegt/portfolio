cd7cbdfb2491e3dd96e6cb418173180edc076e0c
Stijn van der Vegt <stijnvandervegt@gmail.com>
2016-02-02 12:24:15 +0100

    update projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 var Vue = require('vue');
 var nav = require('./components/nav');
-    var portfolio = require('./components/portfolio');
-
+var portfolio = require('./components/portfolio');
 
 new Vue({
     el: '#app',
